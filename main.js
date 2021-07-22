3446f536f111276da1c37c6e4d36366076f55515
Yusuf Ismail <Ismaily@carleton.edu>
2021-07-22 14:51:38 -0500

    radios + input

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-console.log("S");
-document.getElementById("input").style.color = "blue";
+const express = require ('express');
+const app = express();
+app.use(express.urlencoded({extended : true}));
+app.set("view engine", "ejs")
+
+app.get("/" ,(req,res)=>{
+    res.render("index");
+})
+app.post("/", (req,res)=>{
+    console.log(req.body);
+    res.redirect('post-submission');   
+}) 
+app.get('/post-submission', (req,res)=>{
+    res.render('postSubmission');
+})
+
+app.listen(5500,()=>{console.log("listening on port 5500")});

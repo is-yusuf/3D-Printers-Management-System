cd7ed0bd3ea4cd7e52ca5d4a40f0d25edea8e7bd
Yusuf Ismail <Ismaily@carleton.edu>
2021-07-23 11:13:14 -0500

    adding calender file

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 
-app.get("/" ,(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index");
+
+    console.log('accessing');
+
+    app.get(' https://www.googleapis.com/calendar/v3//calendars/calendarId/events/ismaily@carleton.edu/events', (req, res) => {
+        console.log('accessing');
+        console.log(req.body);
+    })
 })
-app.post("/", (req,res)=>{
+app.post("/", (req, res) => {
     console.log(req.body);
-    res.redirect('post-submission');   
-}) 
-app.get('/post-submission', (req,res)=>{
+    res.redirect('post-submission');
+})
+app.get('/post-submission', (req, res) => {
     res.render('postSubmission');
 })
 
-app.listen(5500,()=>{console.log("listening on port 5500")});
+app.listen(5500, () => { console.log("listening on port 5500") });

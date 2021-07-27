9f5ddf459e27c0fe070c6efc0af3cd368b65c0bd
Yusuf Ismail <Ismaily@carleton.edu>
2021-07-27 09:42:47 -0500

    deciding if we're going to host

--- Calender.js
+++ Calender.js
@@ -1,63 +1,88 @@
-const { google } = require('googleapis')
-const { OAuth2 } = google.auth
-const credentials = require('./Credentials.json');
+class cal {
+    constructor() {
+        const { google } = import("googleapis");
+        const { OAuth2 } = import("google.auth");
 
-const oAuth2Client = new OAuth2(
-    credentials.clientID,
-    credentials.clientSecret
-)
-oAuth2Client.setCredentials({
-    refresh_token: '1//04YA_tHCNgrwfCgYIARAAGAQSNwF-L9IrWYRtinxD0fWUEYIcsq1zdX9VoYXk3fOcqBsMlPAqxpxfDEsBj3kpjN7GYd6M93yApRU',
-})
-const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
-const eventStartTime = new Date()
-const eventEndTime = new Date()
-eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+        const credentials = import("./assets/Credentials.json");
 
-const event = {
-    summary: `Meeting with David`,
-    location: `3595 California St, San Francisco, CA 94118`,
-    description: `Meet with David to talk about the new client project and how to integrate the calendar for booking.`,
-    colorId: 1,
-    start: {
-        dateTime: eventStartTime,
-        timeZone: 'America/Chicago',
-    },
-    end: {
-        dateTime: eventEndTime,
-        timeZone: 'America/Chicago',
-    },
-}
-calendar.freebusy.query(
-    {
-        resource: {
-            timeMin: eventStartTime,
-            timeMax: eventEndTime,
-            timeZone: 'America/Minneapolis',
-            items: [{ id: 'primary' }],
-        },
-    },
-    (err, res) => {
-        // Check for errors in our query and log them if they exist.
-        if (err) return console.error('Free Busy Query Error: ', err)
 
-        // Create an array of all events on our calendar during that time.
-        const eventArr = res.data.calendars.primary.busy
+        const oAuth2Client = new OAuth2(
+            credentials.clientID,
+            credentials.clientSecret
+        )
+        oAuth2Client.setCredentials({
+            refresh_token: '1//04YA_tHCNgrwfCgYIARAAGAQSNwF-L9IrWYRtinxD0fWUEYIcsq1zdX9VoYXk3fOcqBsMlPAqxpxfDEsBj3kpjN7GYd6M93yApRU',
+        })
+        const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
-        // Check if event array is empty which means we are not busy
-        if (eventArr.length === 0)
-            // If we are not busy create a new calendar event.
-            return calendar.events.insert(
-                { calendarId: 'primary', resource: event },
-                err => {
-                    // Check for errors and log them if they exist.
-                    if (err) return console.error('Error Creating Calender Event:', err)
-                    // Else log that the event was created.
-                    return console.log('Calendar event successfully created.')
-                }
-            )
+        // const eventStartTime = new Date()
+        // const eventEndTime = new Date()
+        // eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
 
-        // If event array is not empty log that we are busy.
-        return console.log(`Sorry I'm busy...`)
+        const event = {
+            summary: `Scheduled print`,
+            location: `Makerspace`,
+            description: `description.`,
+            colorId: 1,
+            start: {
+                dateTime: null,
+                timeZone: 'America/Chicago',
+            },
+            end: {
+                dateTime: null,
+                timeZone: 'America/Chicago',
+            },
+        }
+
     }
-)
+
+    check(startDate, duration) {
+        let eventStartTime = startDate;
+        let eventEndTime = eventStartTime.setMinutes(eventStartTime.getMinutes() + duration);
+
+        calendar.freebusy.query(
+            {
+                resource: {
+                    timeMin: eventStartTime,
+                    timeMax: eventEndTime,
+                    timeZone: 'America/Minneapolis',
+                    items: [{ id: 'primary' }],
+                },
+            },
+            (err, res) => {
+                // Check for errors in our query and log them if they exist.
+                if (err) return console.error('Free Busy Query Error: ', err)
+
+                // Create an array of all events on our calendar during that time.
+                const eventArr = res.data.calendars.primary.busy
+
+                // Check if event array is empty which means we are not busy
+                if (eventArr.length === 0)
+                    // If we are not busy create a new calendar event.
+                    return true
+                // If event array is not empty log that we are busy.
+                console.log(`Sorry I'm busy...`);
+                return false;
+            }
+        )
+    }
+    schedule(startDate, duration) {
+
+        let eventStartTime = startDate;
+        let eventEndTime = eventStartTime.setMinutes(eventStartTime.getMinutes() + 45);
+        this.event.start.dateTime = eventStartTime;
+        this.event.end.dateTime = eventEndTime;
+        calendar.events.insert(
+            { calendarId: 'primary', resource: this.event },
+            err => {
+                // Check for errors and log them if they exist.
+                if (err) return console.error('Error Creating Calender Event:', err)
+                // Else log that the event was created.
+                return console.log('Calendar event successfully created.')
+            }
+        )
+    }
+}
+
+export { cal };
+// exports.cal = cal;
--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+import { cal } from "./Calender.js";
+
+const calender = new cal();
+oAuth2Client.setCredentials({
+    refresh_token: '1//04YA_tHCNgrwfCgYIARAAGAQSNwF-L9IrWYRtinxD0fWUEYIcsq1zdX9VoYXk3fOcqBsMlPAqxpxfDEsBj3kpjN7GYd6M93yApRU',
+})
+
 function DisplayResults() {
-    console.log('submitted')
-    console.log(document.getElementById('size').value)
+    let material = document.getElementById('material').value
+    let size = document.getElementById('size').value
+    // do the calculation using data collected
+    let output = document.getElementById('printer');
+    let image = document.getElementById('printerimg');
+    output.innerHTML = "this is a placeholder for the printer";
+    image.src = "./assets/oofa.png";
+}
 
-
+function checkSchedule() {
+    let date = document.getElementById('date').value
+    let duration = document.getElementById('duration').value
+    console.log(date);
 }

64ee0a54c8a191174d8189623cf3db7a58d146f9
Yusuf Ismail <Ismaily@carleton.edu>
2021-07-29 11:22:55 -0500

    working version with resfromcal = undefined

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 const { cal } = require("./Calender");
 const { dataflow } = require('googleapis/build/src/apis/dataflow');
+const { Console } = require('console');
 app.use(express.static(__dirname + '/views'));
 app.use(express.json());
-const calendar = new cal();
+const myCalendar = new cal();
 
 app.get("/", (req, res) => {
     res.render("index.html");
@@ -18,40 +19,48 @@ app.post("/check", (req, res) => {
     let date = new Date(req.body.date);
     date.setHours(8);
     let duration = req.body.duration;
-    findSpot(date, duration).then((result) => {
-        console.log({ result })
-        if (result.exists) {
-            res.send({ message: `Successful, we found a spot at ${result.date}` });
-        }
-    })
+    let result = findSpot(date, duration)
+    // console.log({ result })
+    // if (result.exists) {
+    //     res.send({ message: `Successful, we found a spot at ${result.date}` });
+    // }
 })
-async function findSpot(date, duration) {
+
+function findSpot(date, duration) {
     console.log("actualls inside")
 
-    return await calendar.freeBusyStatus(date, duration).then((resultFromCalender) => {
-        console.log({ resultFromCalender })
-
-        return {
-            exists: true,
-            date: date
-        }
-        if (resultFromCalender.exists) {
-            return {
-                exists: true,
-                date: date
-            }
-        }
-        // else if (date.getHours() < 17) {
-        //     date.setMinutes(date.getMinutes() + duration)
-        //     return findSpot(date, duration)
-        // }
-        // else {
-        //     return {
-        //         exists: false,
-        //         date: null
-        //     }
-        // }
+    myCalendar.freeBusyStatus(date, duration).then((resfromcal) => {
+        console.log({ resfromcal });
     })
+    return {
+        exists: true,
+        date, date
+    }
+
+    // .then((resultFromCalender) => {
+    //     console.log({ resultFromCalender })
+
+    //     return {
+    //         exists: true,
+    //         date: date
+    //     }
+    //     if (resultFromCalender.exists) {
+    //         return {
+    //             exists: true,
+    //             date: date
+    //         }
+    //     }
+    // else if (date.getHours() < 17) {
+    //     date.setMinutes(date.getMinutes() + duration)
+    //     return findSpot(date, duration)
+    // }
+    // else {
+    //     return {
+    //         exists: false,
+    //         date: null
+    //     }
+    // }
+    // })
 }
 
 app.listen(5500, () => { console.log("listening on port 5500") });

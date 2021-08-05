e2183399e9c8532b2a52700a86f774af8a71d030
Yusuf Ismail <Ismaily@carleton.edu>
2021-08-05 05:50:41 -0500

    documenting code

--- Calender.js
+++ Calender.js
@@ -4,15 +4,14 @@ class cal {
     constructor() {
 
     }
-    authorize(date) {
-        this.generalDate = date;
-        this.startDate = new Date(date);
-        this.endDay = new Date(date)
-        this.endDay.setMinutes(this.endDay.getMinutes() + 60 * 16 - 1);
+    /**
+     * Authorizes and creates the auth object.
+     */
+    authorize() {
         this.scopes = 'https://www.googleapis.com/auth/calendar';
         this.privateKey = credentials.private_key;
         this.clientEmail = credentials.client_email;
-        // this.projectNum = "<1046718051743>"
+        this.spots = []
         this.calendarId = "c_jkea5jm4ajhefe5ot1ejnsv788@group.calendar.google.com"
         this.auth = new google.auth.JWT(
             this.clientEmail,
@@ -20,25 +19,37 @@ class cal {
             this.privateKey,
             this.scopes
         );
-        this.check = {
-            auth: this.auth,
-            resource: {
-                timeMin: this.startDate,
-                timeMax: this.endDay,
-                timeZone: "America/Chicago",
-                items: [{ id: this.calendarId }]
-            }
-        }
+
         this.calendar = google.calendar({
             version: 'v3',
             project: this.projectNum,
             auth: this.auth
         });
-        this.spots = []
 
     }
-
-
+    /**
+     * Sets the general date of Calendar and generates request object.
+     * @param {date} date date object to set the day of the Calendar. 
+     */
+    setDates(date) {
+        this.generalDate = new Date(date);
+        this.endDay = new Date(date)
+        this.endDay.setMinutes(this.endDay.getMinutes() + 60 * 16 - 1);
+        this.check = {
+            auth: this.auth,
+            resource: {
+                timeMin: this.generalDate,
+                timeMax: this.endDay,
+                timeZone: "America/Chicago",
+                items: [{ id: this.calendarId }]
+            }
+        }
+    }
+    /**
+     * Loops over the day by 30 minutes steps to find the first slot.
+     * @param {Number} durationMinutes The duration of the event to find the first slot for. 
+     * @returns {date,date} the start and end dates of the event.
+     */
     findFirstSpot(durationMinutes) {
         this.slotstart = new Date(this.generalDate);
         this.slotend = new Date(this.generalDate);
@@ -50,8 +61,17 @@ class cal {
                 return false;
             }
         }
+        console.log({ start: this.slotstart, end: this.slotend })
         return { start: this.slotstart, end: this.slotend }
     }
+    /**
+     * 
+     * @param {date} s_s Slot Start 
+     * @param {date} s_e Slot End
+     * @param {date} e_s Event Start
+     * @param {date} e_e Event End
+     * @returns {boolean} true if the event and slot overlaps, false otherwise.
+     */
     overlaps(s_s, s_e, e_s, e_e) {
         s_s = s_s.getTime()
         s_e = s_e.getTime()
@@ -64,24 +84,34 @@ class cal {
             return true
         }
     }
+    /**
+     * convers the array of events (originally of strings) to an array of dates.
+     */
     convertEventArr() {
         this.eventArr.forEach((startend, index) => {
-
             this.eventArr[index].start = new Date(startend.start);
-            // this.eventArr[index].start.setHours(this.eventArr[index].start.getHours() - 5)
             this.eventArr[index].end = new Date(startend.end);
-            // this.eventArr[index].end.setHours(this.eventArr[index].end.getHours() - 5)
-
         })
     }
+    /**
+     * 
+     * @param {date} date the date from which to grab the events. 
+     * @returns {calendar} the ready calendar object with attributes set.
+     */
     async freeBusyStatus(date) {
-        this.authorize(date);
+        this.authorize();
+        this.setDates(date);
         return this.calendar.freebusy.query(this.check).then((response) => {
             this.eventArr = response.data.calendars["c_jkea5jm4ajhefe5ot1ejnsv788@group.calendar.google.com"].busy;
             this.convertEventArr()
             return this;
         })
     }
+    /**
+     * Schedules and event in a specefic time slot.
+     * @param {Date} startDate the start date of the event to schedule  
+     * @param {Date} endDate the end date of the event to schedule
+     */
     schedule(startDate, endDate) {
         this.event = {
             summary: `reserved`,
@@ -105,6 +135,12 @@ class cal {
             }
         )
     }
+    /**
+     * checks if a slot is empty or not
+     * @param {Date} startDate the start date of the slot to check 
+     * @param {Date} endDate the end date of the slot to check
+     * @returns {boolean} true if empty, false if used.
+     */
     checkExactDate(startDate, endDate) {
         if (this.eventArr.length == 0) {
             return true
@@ -120,11 +156,19 @@ class cal {
         }
         return false;
     }
+    /**
+     * Prints the start and end hours and minutes to the terminal in human friendly way without the date.
+     * @param {Date} startdate the start of the date 
+     * @param {Date} enddate the end of the date
+     */
     printTime(startdate, enddate) {
         console.log({ start: `${startdate.getHours()} : ${startdate.getMinutes()}` })
         console.log({ end: `${enddate.getHours()} : ${enddate.getMinutes()}` })
 
     }
+    /**
+     * Prints the events in that day.
+     */
     printEvents() {
         console.log({ eventarr: this.eventArr })
     }

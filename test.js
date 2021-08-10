951b6e9424e8e5b3960b3b25949cea476dd94d61
Yusuf Ismail <Ismaily@carleton.edu>
2021-08-10 14:34:48 -0500

    styling

--- test.js
+++ test.js
@@ -1,25 +1,22 @@
-let slot_s = new Date("2021-07-30T19:00:00.000Z")
-let slot_e = new Date("2021-07-30T20:30:00.000Z")
-let event_s = new Date("2021-07-30T14:30:00.000Z")
-let event_e = new Date("2021-07-30T18:30:00.000Z")
-
-// console.log(x, y, z, f)
 
 
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'is.yusuf.tester@gmail.com',
 
+        pass: '01524325232'
 
-console.log(overlaps(slot_s, slot_e, event_s, event_e))
-
-function overlaps(s_s, s_e, e_s, e_e) {
-    s_s = s_s.getTime()
-    s_e = s_e.getTime()
-    e_s = e_s.getTime()
-    e_e = e_e.getTime()
-    if (s_s > e_e || s_e < e_s) {
-        return false
     }
+});
 
-    else {
-        return true
-    }
-}
+
+
+console.log('created');
+transporter.sendMail({
+    from: 'is.yusuf.tester@gmail.com',
+    to: 'phy.ysf@gmail.com',
+    subject: 'hello world!',
+    text: 'hello world!'
+});
